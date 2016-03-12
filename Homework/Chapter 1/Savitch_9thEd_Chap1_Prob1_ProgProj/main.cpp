916b5577cb1077a839eb2d1eb44b89c537223d11
jcervantes2113 <josecervantes678@gmail.com>
2016-03-12 07:55:43 -0800

    CSC-5

---
+++ Homework/Chapter 1/Savitch_9thEd_Chap1_Prob1_ProgProj/main.cpp
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: joseluiscervantes
+ *
+ * Created on March 12, 2016, 7:51 AM
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begin Here!
+
+int main(int argc, char** argv) {
+    //Declare Variables
+
+    
+    
+    //Initialize Variables
+    
+    //input values
+    
+    //Map the inputs to the outputs
+    
+    //Output the results
+    
+    //This program display this image.
+    cout<<"This program creates this\n";
+    cout<<"\n";
+    cout<<" ************************************************\n";
+    cout<<"          C C C           S S S         !!\n";
+    cout<<"         C      C       S       S       !!\n";
+    cout<<"        C              S                !!\n";
+    cout<<"       C                S               !!\n";
+    cout<<"       C                 S S S S        !!\n";
+    cout<<"       C                         S      !!\n";
+    cout<<"        C                         S     !!\n";
+    cout<<"         C      C        S       S        \n";
+    cout<<"          C C C           S S S S       00\n";
+    cout<<" ************************************************";
+    cout<<"     Computer Science is Cool Stuff!!!\n";
+    
+    //Exit Stage Right
+    return 0;
+}
