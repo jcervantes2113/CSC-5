2d79f2a0de7a843b85aa809a1bf2346090ac54c4
jcervantes2113 <josecervantes678@gmail.com>
2016-03-14 11:00:45 -0700

    CSC-5

---
+++ Lab/True_&_False_Chart_LAB_/main.cpp
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: joseluiscervantes
+ *
+ * Created on March 14, 2016, 9:42 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+    //Global Constants
+
+//Function  Prototypes
+
+//Execution Begins Here
+
+
+
+
+//Initialize Variables
+
+
+//Input Values 
+
+
+    
+//Map The Inputs to the Outputs
+
+    
+//Output the Results
+int main(int argc, char** argv) {
+    bool x = true;
+    bool y = true;
+    bool num = x&&y;
+    
+    
+    
+   
+    
+
+    
+    
+    cout <<"x   y   !x   !y   x&&y   x||y   x^y   x^y^y   x^y^x   !(x&&y)   !x||!y   !(x||y)   !x&&!y " << endl;
+    
+    cout << (x?'T':'F');
+    cout <<"   " << (y? 'T':'F'); 
+    cout <<"    "<< (!x? 'T':'F');
+    cout <<"    "<<(!y? 'T':'F');
+    cout <<"     "<<(x&&y? 'T':'F');
+    cout <<"     "<< (x||y? 'T':'F');
+    cout <<"      "<<(x^y? 'T':'F');
+    cout <<"      "<< (x^y^y? 'T':'F');
+    cout <<"       "<<(x^y^x? 'T':'F');
+    cout <<"         "<<(!(x&&y)? 'T':'F');
+    cout <<"        " <<(!x||!y? 'T':'F');
+    cout <<"        " <<(!(x||y)? 'T':'F');
+    cout <<"         "<<(!x&&!y? 'T':'F') << endl;
+    
+    y = false;
+    x = true;
+    
+    cout << (x?'T':'F');
+    cout <<"   " << (y? 'T':'F'); 
+    cout <<"    "<< (!x? 'T':'F');
+    cout <<"    "<<(!y? 'T':'F');
+    cout <<"     "<<(x&&y? 'T':'F');
+    cout <<"     "<< (x||y? 'T':'F');
+    cout <<"      "<<(x^y? 'T':'F');
+    cout <<"      "<< (x^y^y? 'T':'F');
+    cout <<"       "<<(x^y^x? 'T':'F');
+    cout <<"         "<<(!(x&&y)? 'T':'F');
+    cout <<"        " <<(!x||!y? 'T':'F');
+    cout <<"        " <<(!(x||y)? 'T':'F');
+    cout <<"         "<<(!x&&!y? 'T':'F') << endl;
+    
+    x = false;
+    y = true;
+
+    cout << (x?'T':'F');
+    cout <<"   " << (y? 'T':'F'); 
+    cout <<"    "<< (!x? 'T':'F');
+    cout <<"    "<<(!y? 'T':'F');
+    cout <<"     "<<(x&&y? 'T':'F');
+    cout <<"     "<< (x||y? 'T':'F');
+    cout <<"      "<<(x^y? 'T':'F');
+    cout <<"      "<< (x^y^y? 'T':'F');
+    cout <<"       "<<(x^y^x? 'T':'F');
+    cout <<"         "<<(!(x&&y)? 'T':'F');
+    cout <<"        " <<(!x||!y? 'T':'F');
+    cout <<"        " <<(!(x||y)? 'T':'F');
+    cout <<"         "<<(!x&&!y? 'T':'F') << endl;
+    
+    x = false;
+    y = false;
+    
+    cout << (x?'T':'F');
+    cout <<"   " << (y? 'T':'F'); 
+    cout <<"    "<< (!x? 'T':'F');
+    cout <<"    "<<(!y? 'T':'F');
+    cout <<"     "<<(x&&y? 'T':'F');
+    cout <<"     "<< (x||y? 'T':'F');
+    cout <<"      "<<(x^y? 'T':'F');
+    cout <<"      "<< (x^y^y? 'T':'F');
+    cout <<"       "<<(x^y^x? 'T':'F');
+    cout <<"         "<<(!(x&&y)? 'T':'F');
+    cout <<"        " <<(!x||!y? 'T':'F');
+    cout <<"        " <<(!(x||y)? 'T':'F');
+    cout <<"         "<<(!x&&!y? 'T':'F') << endl;
+    
+    
+
+    
+    
+//Exit Stage Right!
+    
+ 
+    
+    //second code
+    
+    
+
+    return 0;
+}
+
