13fe13abb9c2638ab6b44c53f527a7795156dd62
jcervantes2113 <josecervantes678@gmail.com>
2016-04-04 10:00:26 -0700

    CSC - 5 42829

--- Homework/Cervantes, Jose - Assignment 4 - 42829/Gaddis_8thEd_Chap5_Prob06_Prog_Challenges/main.cpp
+++ Homework/Cervantes, Jose - Assignment 4 - 42829/Gaddis_8thEd_Chap5_Prob06_Prog_Challenges/main.cpp
@@ -7,7 +7,7 @@
 /* 
  * File:   main.cpp
  * Author: joseluiscervantes
- *
+ * Purpose: Calculate the amount of miles traveled over a given amount of time
  * Created on April 3, 2016, 11:01 PM
  */
 
@@ -23,15 +23,33 @@ using namespace std;
 
 int main(int argc, char** argv) {
 //Declared Variables
+    float distnce, speed, time, intrval;
 
 //Initialize Variables
+    intrval = 1;
+    
 
 //Input Values 
+    cout << "This program calculates the distance traveled over a given amount \n"
+            "of time." << endl << endl;
+    cout << "Please enter the miles traveled over a period of an hour: ";
+    cin  >> speed;
+    cout << "Please enter the amount of hours that you traveled: ";
+    cin  >> time;
     
 //Map The Inputs to the Outputs
+    cout << "Hours    Distance Travled" << endl;
+    while (intrval <= time){
+        
+        //This calculates the distance traveled at each hour and also displays 
+        //it.
+        distnce = speed * intrval;
+        cout << intrval <<"        " <<distnce << endl;
+        intrval++;
+    }
     
 //Output the Results
-
+   
 //Exit Stage Right!
     return 0;
 }
